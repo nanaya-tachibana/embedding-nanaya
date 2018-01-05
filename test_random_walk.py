733d15fdc6b710115d88479ab27c776e5099e795
nanaya <nanaya100@gmail.com>
2018-01-05 11:27:27 +0800

    Add neu(https://github.com/thunlp/NEU/). Add a test for deep walk.

--- test_random_walk.py
+++ test_random_walk.py
@@ -5,4 +5,4 @@ from corpus import RandomWalkCorpus
 if __name__ == '__main__':
     g = nx.read_edgelist('example_graphs/p2p-Gnutella08.edgelist')
     builder = RandomWalkCorpus(g)
-    builder.build_corpus(80, 10, 0, '.', n_jobs=4)
+    builder.build_corpus(80, 10, output_file='p2p.txt', n_jobs=4)

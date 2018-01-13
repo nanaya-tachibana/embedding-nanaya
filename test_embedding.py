ff0ba68f2b0436563a24469a102e0da96726d365
nanaya <nanaya100@gmail.com>
2018-01-13 16:04:10 +0800

    Add test file.

--- test_embedding.py
+++ test_embedding.py
@@ -1,6 +1,9 @@
 import os
 import time
+import shutil
+from zipfile import ZipFile
 
+import requests
 from gensim import logging
 from gensim.models import Word2Vec, KeyedVectors
 from gensim.models.word2vec import LineSentence
@@ -9,6 +12,18 @@ from embedding import Word2vec
 
 
 if __name__ == '__main__':
+    filename = 'example_text/text8'
+    if not os.path.exists(filename):
+        print('Download test file.')
+        url = 'http://mattmahoney.net/dc/text8.zip'
+        zip_file = '.'.join([filename, 'zip'])
+        with requests.get(url, stream=True) as r:
+            with open(zip_file, 'wb') as f:
+                shutil.copyfileobj(r.raw, f)
+        with ZipFile(zip_file, 'r') as zf:
+            zf.extract('text8', path=os.path.dirname(filename))
+        os.rm(zip_file)
+
     model = Word2vec(cbow=1,
                      embedding_size=200,
                      window=8,
@@ -19,7 +34,7 @@ if __name__ == '__main__':
                      iters=15)
 
     now = time.time()
-    model.train('example_text/text8', n_jobs=os.cpu_count())
+    model.train(filename, n_jobs=os.cpu_count())
     print('Training compelete. Total time ', time.time() - now)
     model.save_word2vec_format('vec.bin')
 
